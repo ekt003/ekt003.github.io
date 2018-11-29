fc7ffeb50615a23f4d9084072ef42d7aff9634fe
Emily Turner <ekt6170@g.rit.edu>
2018-11-28 19:27:28 -0500

    Placeholder images and time stuff

--- src/webFunctionality.js
+++ src/webFunctionality.js
@@ -4,35 +4,79 @@ let now = new Date();
 
 let hour = now.getHours();
 
-let backgroundIMG = document.querySelectorAll("#title");
-
+//all of the things to be changed based on time of day
+let backgroundIMG = document.querySelector("#title");
+let activeLink = document.querySelector('.active');
+let titleScreen = document.querySelector('#title');
+let sectionTitle = document.querySelector('#sectionTitle');
+let navBar = document.querySelector('#navbarDiv');
+let formButt = document.querySelector('#formButton');
 
 //sets the background image based on the local time of day
 //sunrise from 5AM to 9AM
 if(hour>=5 && hour<9){
-    for(let div of backgroundIMG){
-        div.style.backgroundImage = "url('sunrise.png')";
+    if(backgroundIMG){
+        backgroundIMG.style.backgroundImage = "url('background.svg')";
+        titleScreen.style.textShadow = "#0077aa 2.5px 2.5px";
+        titleScreen.style.backgroundColor = "#0077aa";
     }
+    if(sectionTitle){
+        sectionTitle.style.backgroundImage = "url('background.svg')";
+        sectionTitle.style.textShadow = "#0077aa 2.5px 2.5px";
+        sectionTitle.style.backgroundColor = "#0077aa";
+    }
+    if(formButt){
+        formButt.style.backgroundColor = "#0099cc";
+    }
+    activeLink.style.backgroundColor = "#0099cc";
+    navBar.style.backgroundColor = "#0077aa";
+    
 }
 //day from 9AM to 5PM
 if(hour>=9 && hour<17){
-    for(let div of backgroundIMG){
-        div.style.backgroundImage = "url('background.svg')";
+    if(backgroundIMG){
+        backgroundIMG.style.backgroundImage = "url('background.svg')";
+        titleScreen.style.textShadow = "#0077aa 2.5px 2.5px";
+        titleScreen.style.backgroundColor = "#0077aa";
     }
+    if(sectionTitle){
+        sectionTitle.style.backgroundImage = "url('background.svg')";
+        sectionTitle.style.textShadow = "#0077aa 2.5px 2.5px";
+        sectionTitle.style.backgroundColor = "#0077aa";
+    }
+    if(formButt){
+        formButt.style.backgroundColor = "#0099cc";
+    }
+    activeLink.style.backgroundColor = "#0099cc";
+    navBar.style.backgroundColor = "#0077aa";
 }
 
 //sunset from 5PM to 8PM
 if(hour>=17 && hour<20){
-    for(let div of backgroundIMG){
-        div.style.backgroundImage = "url('sunset.png')";
+    if(backgroundIMG){
+        backgroundIMG.style.backgroundImage = "url('background.svg')";
+        titleScreen.style.textShadow = "#0077aa 2.5px 2.5px";
+        titleScreen.style.backgroundColor = "#0077aa";
     }
+    if(sectionTitle){
+        sectionTitle.style.backgroundImage = "url('background.svg')";
+        sectionTitle.style.textShadow = "#0077aa 2.5px 2.5px";
+        sectionTitle.style.backgroundColor = "#0077aa";
+    }
+    if(formButt){
+        formButt.style.backgroundColor = "#0099cc";
+    }
+    activeLink.style.backgroundColor = "#0099cc";
+    navBar.style.backgroundColor = "#0077aa";
 }
 
 //night from 8PM to 5AM
 if((hour>=20 && hour<23) || (hour>=0 && hour<5)){
-    for(let div of backgroundIMG){
-        div.style.backgroundImage = "url('backgroundEvening.svg')";
-    }
+    backgroundIMG.style.backgroundImage = "url('backgroundEvening.svg')";
+    sectionTitle.style.backgroundImage = "url('backgroundEvening.svg')";
+    activeLink.style.backgroundColor = "#0022aa";
+    titleScreen.style.textShadow = "none";
+    navBar.style.backgroundColor = "#0022aa";
 }
 
 
@@ -41,7 +85,10 @@ let i=0;
 let txt = 'Dev, Designer, Dork';
 let speed = 100;
 
-typeWriter();
+if(backgroundIMG){
+    typeWriter();
+}
+
 
 function typeWriter() {
   if (i < txt.length) {
