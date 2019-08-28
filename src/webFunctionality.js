b5df5eec773fff5ae0ea8eed1d6434cee8d03442
Emily Turner <ekt6170@g.rit.edu>
2019-08-27 21:07:01 -0400

    Adding sunset time of day, other minor changes
    Project pages coming soon! (I hope!)

--- src/webFunctionality.js
+++ src/webFunctionality.js
@@ -1,20 +1,39 @@
  "use strict";
-//gets the date and time of being accessed
-let now = new Date();
 
-let hour = now.getHours();
+
+window.onload = init;
+
+function init(){
+    //gets the date and time of being accessed
+    let now = new Date();
+
+    let hour = now.getHours();
+
+    let styleSheet= document.querySelector("#theme");
+    
+
+    //day from 9AM to 4PM
+    if(hour>=5 && hour<16){
+      styleSheet.setAttribute("href", "css/themes/day.css");
+    }
+
+
+    //sunset from 4PM to 8PM
+    if(hour>=16 && hour<20){
+      styleSheet.setAttribute("href", "css/themes/sunset.css");
+    }
+
+    //night from 8PM to 5AM
+    if((hour>=20 && hour<=23) || (hour>=0 && hour<5)){
+      styleSheet.setAttribute("href", "css/themes/night.css");
+    }
+
+}
+
+
 
 //all of the things to be changed based on time of day
 let backgroundIMG = document.querySelector("#title");
-let activeLink = document.querySelector('.active');
-let titleScreen = document.querySelector('#title');
-let sectionTitle = document.querySelector('#sectionTitle');
-let navBar = document.querySelector('#navbarDiv');
-let formButt = document.querySelector('#formButton');
-let footer = document.querySelector('#footer');
-let profileIMG = document.querySelector('#profile');
-let resume = document.querySelector('#resume');
-let links = document.querySelectorAll('.link');
 
 //sets the background image based on the local time of day
 //sunrise from 5AM to 9AM
@@ -47,35 +66,6 @@ let links = document.querySelectorAll('.link');
 //     footer.style.backgroundColor = "#0077aa";
     
 // }
-//day from 9AM to 4PM
-if(hour>=5 && hour<20){
-    if(backgroundIMG){
-        backgroundIMG.style.backgroundImage = "url('background.svg')";
-        titleScreen.style.textShadow = "#0077aa 2.5px 2.5px";
-        titleScreen.style.backgroundColor = "#0077aa";
-    }
-    if(sectionTitle){
-        sectionTitle.style.backgroundImage = "url('background.svg')";
-        sectionTitle.style.textShadow = "#0077aa 2.5px 2.5px";
-        sectionTitle.style.backgroundColor = "#0077aa";
-    }
-    if(formButt){
-        formButt.style.backgroundColor = "#0099cc";
-    }
-    if(profileIMG){
-        profileIMG.style.backgroundColor = "#0099cc";
-        profileIMG.style.border="5px solid #0077aa";
-    }
-    if(resume){
-        resume.style.backgroundColor = "#0099cc";
-    }
-    for(let link of links){
-        link.style.color="#0077aa";
-    }
-    activeLink.style.backgroundColor = "#0099cc";
-    navBar.style.backgroundColor = "#0077aa";
-    footer.style.backgroundColor = "#0077aa";
-}
 
 // //sunset from 4PM to 8PM
 // if(hour>=16 && hour<20){
@@ -107,35 +97,7 @@ if(hour>=5 && hour<20){
 //     footer.style.backgroundColor = "#ff89ac";
 // }
 
-//night from 8PM to 5AM
-if((hour>=20 && hour<23) || (hour>=0 && hour<5)){
-    if(backgroundIMG){
-        backgroundIMG.style.backgroundImage = "url('backgroundEvening.svg')";
-        titleScreen.style.textShadow = "none";
-        titleScreen.style.backgroundColor = "#0022aa";
-    }
-    if(sectionTitle){
-        sectionTitle.style.backgroundImage = "url('backgroundEvening.svg')";
-        sectionTitle.style.textShadow = "none";
-        sectionTitle.style.backgroundColor = "#0022aa";
-    }
-    if(formButt){
-        formButt.style.backgroundColor = "#0044cc";
-    }
-    if(profileIMG){
-        profileIMG.style.backgroundColor = "#0022aa";
-        profileIMG.style.border="5px solid #0044cc";
-    }
-    if(resume){
-        resume.style.backgroundColor = "#0022aa";
-    }
-    for(let link of links){
-        link.style.color="#0022aa";
-    }
-    activeLink.style.backgroundColor = "#0044cc";
-    navBar.style.backgroundColor = "#0022aa";
-    footer.style.backgroundColor = "#0022aa";
-}
+
 
 
 // JS Typewriter code from W3Schools: https://www.w3schools.com/howto/howto_js_typewriter.asp
