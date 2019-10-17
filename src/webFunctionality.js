a213e918c5c9ab642089462dbdf73a18862b1714
Emily Turner <ekt6170@g.rit.edu>
2019-10-17 18:02:03 -0400

    sunrise functionality implemented

--- src/webFunctionality.js
+++ src/webFunctionality.js
@@ -11,9 +11,13 @@ function init(){
 
     let styleSheet= document.querySelector("#theme");
     
-
+    //sunrise from 4AM to 9AM
+    if(hour>=4 && hour<9){
+      styleSheet.setAttribute("href", "css/themes/sunrise.css");
+    }
+       
     //day from 9AM to 4PM
-    if(hour>=5 && hour<16){
+    if(hour>=9 && hour<16){
       styleSheet.setAttribute("href", "css/themes/day.css");
     }
 
@@ -24,81 +28,11 @@ function init(){
     }
 
     //night from 8PM to 5AM
-    if((hour>=20 && hour<=23) || (hour>=0 && hour<5)){
+    if((hour>=20 && hour<=23) || (hour>=0 && hour<4)){
       styleSheet.setAttribute("href", "css/themes/night.css");
     }
 
 }
-
-
-
-//all of the things to be changed based on time of day
-let backgroundIMG = document.querySelector("#title");
-
-//sets the background image based on the local time of day
-//sunrise from 5AM to 9AM
-// if(hour>=5 && hour<9){
-//     if(backgroundIMG){
-//         backgroundIMG.style.backgroundImage = "url('background.svg')";
-//         titleScreen.style.textShadow = "#0077aa 2.5px 2.5px";
-//         titleScreen.style.backgroundColor = "#0077aa";
-//     }
-//     if(sectionTitle){
-//         sectionTitle.style.backgroundImage = "url('background.svg')";
-//         sectionTitle.style.textShadow = "#0077aa 2.5px 2.5px";
-//         sectionTitle.style.backgroundColor = "#0077aa";
-//     }
-//     if(formButt){
-//         formButt.style.backgroundColor = "#0099cc";
-//     }
-//     if(profileIMG){
-//         profileIMG.style.backgroundColor = "#0099cc";
-//         profileIMG.style.border="5px solid #0077aa";
-//     }
-//     if(resume){
-//         resume.style.backgroundColor = "#0099cc";
-//     }
-//     for(let link of links){
-//         link.style.color="#0077aa";
-//     }
-//     activeLink.style.backgroundColor = "#0099cc";
-//     navBar.style.backgroundColor = "#0077aa";
-//     footer.style.backgroundColor = "#0077aa";
-    
-// }
-
-// //sunset from 4PM to 8PM
-// if(hour>=16 && hour<20){
-//     if(backgroundIMG){
-//         backgroundIMG.style.backgroundImage = "url('background.svg')";
-//         titleScreen.style.textShadow = "#ff89ac 2.5px 2.5px";
-//         titleScreen.style.backgroundColor = "#ff89ac";
-//     }
-//     if(sectionTitle){
-//         sectionTitle.style.backgroundImage = "url('background.svg')";
-//         sectionTitle.style.textShadow = "#ff89ac 2.5px 2.5px";
-//         sectionTitle.style.backgroundColor = "#ff89ac";
-//     }
-//     if(formButt){
-//         formButt.style.backgroundColor = "#ff89ac";
-//     }
-//     if(profileIMG){
-//         profileIMG.style.backgroundColor = "#ff89ac";
-//         profileIMG.style.border="5px solid #ff89ac";
-//     }
-//     if(resume){
-//         resume.style.backgroundColor = "#ffabcd";
-//     }
-//     for(let link of links){
-//         link.style.color="#ff89ac";
-//     }
-//     activeLink.style.backgroundColor = "#ffabcd";
-//     navBar.style.backgroundColor = "#ff89ac";
-//     footer.style.backgroundColor = "#ff89ac";
-// }
-
-
-
 
 // JS Typewriter code from W3Schools: https://www.w3schools.com/howto/howto_js_typewriter.asp
 let i=0;
